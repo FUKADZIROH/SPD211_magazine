4f57cf006582e191533b30f89c391e5759ff9725
snizhana28 <snizhana2812kostyuk@gmail.com>
2023-09-30 16:24:21 +0300

    Done Task (LoginScreen)

---
+++ SPD211_magazine/Navigator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPD211_magazine
+{
+    public class Navigator
+    {
+        //function to go to the AdmainScreen
+        public void GoToAdmainScreen()
+        {
+            
+        }
+        //function to go to the UserScreen
+        public void GoToUserScreen()
+        {
+
+        }
+        //function to go to the RegisterScreen
+        public void GoToRegisterScreen()
+        {
+
+        }
+        //function to go to the LoginScreen
+        public void GoToLoginScreen()
+        {
+
+        }
+    }
+}
